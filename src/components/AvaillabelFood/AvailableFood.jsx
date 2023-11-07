162a07432434847e33a8e296e80b9a08ecb7fb84
mashuq0068 <mashuq0068@gmail.com>
2023-11-08 00:44:36 +0600

    solved the 2nd time search problem

--- src/components/AvaillabelFood/AvailableFood.jsx
+++ src/components/AvaillabelFood/AvailableFood.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 const AvailableFood = () => {
   const secureAxios = useAxios();
   const [foods, setFoods] = useState([]);
+  const [filteredFoods, setFilteredFoods] = useState([]); // New state for filtered data
   const [loading, setLoading] = useState(true);
 
   function compareFoods(a, b) {
@@ -16,37 +17,25 @@ const AvailableFood = () => {
   }
 
   const handleSort = () => {
-    const sortedFoods = [...foods].sort(compareFoods);
-    console.log(sortedFoods)
-    setFoods(sortedFoods);
+    const sortedFoods = [...filteredFoods].sort(compareFoods);
+    setFilteredFoods(sortedFoods);
   };
-  const handleSearch = (e) => {
-    e.preventDefault()
-    const search = e.target.search.value
-    
-    const filterByName = [...foods].filter((food) => {
-      
-      if (food.foodName) {
-      
-        
-      
-            return  search.toLowerCase().includes(food.foodName.toLowerCase())
-       
-        
-      }
-      return false;
-    });
-  
-    console.log(filterByName)
-    console.log(search)
-    setFoods(filterByName)
-  
 
-  }
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const search = e.target.search.value;
+    const filterByName = [...foods].filter((food) =>{
+    if(food.foodName){
+     return search.toLowerCase().includes(food.foodName.toLowerCase())
+    }
+  });
+    setFilteredFoods(filterByName);
+  };
 
   useEffect(() => {
     secureAxios.get('/foods').then((res) => {
       setFoods(res.data);
+      setFilteredFoods(res.data); // Initialize filtered data with the original data
       setLoading(false);
     });
   }, []);
@@ -60,19 +49,31 @@ const AvailableFood = () => {
   return (
     <div className="mt-[15vh] px-[6%] pb-[7%]">
       <div className="mb-[10vh]">
-      <div className="text-end flex justify-between">
-        <form  onSubmit={handleSearch} className="flex">
-          <input name="search" type="text" placeholder="Search by Food Name" className="input rounded-r-none md:text-lg text-base  2xl:text-2xl input-bordered drop-shadow-xl  w-[30vw]"/>
-        <input type="submit" className="btn bg-teal-400 capitalize drop-shadow-xl hover:bg-teal-400 border-none text-2xl rounded-l-none" value="Search" />
-        </form>
-      <button onClick={handleSort} className="btn bg-teal-400 capitalize drop-shadow-xl shadow-xl hover:bg-teal-400 border-none text-2xl">
-        Sort By Expired Date
-      </button>
-      </div>
+        <div className="text-end flex justify-between">
+          <form onSubmit={handleSearch} className="flex">
+            <input
+              name="search"
+              type="text"
+              placeholder="Search by Food Name"
+              className="input rounded-r-none md:text-lg text-base 2xl:text-2xl input-bordered drop-shadow-xl w-[30vw]"
+            />
+            <input
+              type="submit"
+              className="btn bg-teal-400 capitalize drop-shadow-xl hover-bg-teal-400 border-none text-2xl rounded-l-none"
+              value="Search"
+            />
+          </form>
+          <button
+            onClick={handleSort}
+            className="btn bg-teal-400 capitalize drop-shadow-xl shadow-xl hover-bg-teal-400 border-none text-2xl"
+          >
+            Sort By Expired Date
+          </button>
+        </div>
       </div>
       <div>
-        <div className="grid lg:grid-cols-3  md:grid-cols-2 grid-cols-1 gap-7">
-          {foods.map((food) => (
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-7">
+          {filteredFoods.map((food) => (
             <OneFood key={food._id} food={food} />
           ))}
         </div>
